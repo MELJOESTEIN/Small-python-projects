86863ab26e1d62d7ca087aff3c18e2a1906ce7ce
MELJOESTEIN <thierrymemel@gmail.com>
2022-08-28 12:46:23 +0000

    new line added

--- math_operation.py
+++ math_operation.py
@@ -1,3 +1,9 @@
+name = input("what is your name ?  ")
+print("Welcome", name,"!")
+
+print("Let's do some maths ! ======================================")
+
+num1 = int(input("Give a number ... "))
 num2 = int(input("Give the second number ..."))
 
 result = num1 + num2
