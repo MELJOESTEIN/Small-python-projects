5e19a184e40b837c1687733debb573daefe9310b
MELJOESTEIN <thierrymemel@gmail.com>
2022-08-28 12:52:07 +0000

    New folder created

---
+++ Math_operations/math_operation.py
@@ -0,0 +1,11 @@
+name = input("what is your name ?  ")
+print("Welcome", name,"!")
+
+print("Let's do some maths ! ======================================")
+
+num1 = int(input("Give a number ... "))
+num2 = int(input("Give the second number ..."))
+
+result = num1 + num2
+
+print (f"The sum of {num1} and {num2} is : ", result)
